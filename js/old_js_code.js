4f0c4483928040bfd9b534a37ca50bb08fec1b07
Dmitry Tkach <kathudawar@gmail.com>
2020-11-01 18:23:53 +0300

    Ninth commit

---
+++ js/old_js_code.js
@@ -0,0 +1,129 @@
+'use strict'
+const btn = document.querySelector('.btn');
+
+function rollDices() {
+    const dicesNum = +document.querySelector('#dices-num').value;
+    const edgesNum = document.querySelector('#edges-num').value;
+    const resultDiv = document.querySelector('.result-div');
+    const min = 0;
+    let result = 0;
+    let resultWrong = 'Error: Enter positive integer num';
+    let arrEdges = [];
+
+// function playAudio {
+    function playAudio () {
+        const audio = new Audio();
+        audio.preload = 'auto';
+        audio.src = './media/sounds/dice_roll_sound.mp3';
+        audio.play();
+    }
+
+
+//function validate FateCore
+    function validateFate(edgesNum) {
+        return ['f', 'fate', 'fatecore', 'ф', 'фейт', 'фейткор'].includes(edgesNum.toLowerCase());
+    }
+
+//create wrapper-div eachResult
+    const eachResult = document.createElement('div');
+    eachResult.classList.add('each-result');
+    resultDiv.appendChild(eachResult);
+
+//function that create input div
+    function createSymbol(symbolName, styleName) {
+        const symbol = document.createElement('div');
+        symbol.classList.add('symbol');
+        symbol.classList.add(styleName);
+        eachResult.appendChild(symbol);
+        symbol.textContent = symbolName;
+    }
+
+//function clear input value
+    function clearInput () {
+        document.querySelector('#dices-num').value = '';
+        document.querySelector('#edges-num').value = '';
+    }
+
+//function for checking validity isInteger
+    function isInteger (num) {
+        return (num % 1 !== 0);
+    }
+
+//check the validity of the input
+    if (dicesNum <=0 || isNaN(dicesNum) || edgesNum <= 0 || isInteger(dicesNum)) {
+        createSymbol(resultWrong, 'symbol','result');
+        clearInput();
+        console.log('Wrong number');
+    } else if (!validateFate(edgesNum) && isInteger(edgesNum)) {
+        createSymbol(resultWrong, 'symbol','result');
+        clearInput();
+        console.log("EdgesNum isn't integer");
+    } else if (!validateFate(edgesNum) && isNaN(edgesNum)) {
+        createSymbol(resultWrong, 'symbol','result');
+        clearInput();
+        console.log('EdgesNum is NaN');
+    } else {
+//FOR FateCore
+        if (validateFate(edgesNum))
+        {
+            console.log('Start FateCore-mode');
+
+//create arr of edges for FateCore
+            for (let i = 0; i < dicesNum; i++) {
+                arrEdges.push(Math.floor(Math.random() * (5 - min +1) + min));
+                console.log('arrEdges[i]' + arrEdges[i]);
+            }
+            console.log('arr-fate before turning into -1, 0, 1: ' + arrEdges);
+
+            for (let y = 0; y < arrEdges.length; y++) {
+
+                if (arrEdges[y] === 0 || arrEdges[y] === 1) {
+                    arrEdges[y] = -1;
+                } else if (arrEdges[y] === 2 || arrEdges[y] === 3) {
+                    arrEdges[y] = 0;
+                } else if (arrEdges[y] === 4 || arrEdges[y] === 5) {
+                    arrEdges[y] = 1;
+                } else {
+                    console.log('Something goes wrong');
+                }
+            }
+            console.log('arr-fate after turning into -1, 0, 1: ' + arrEdges);
+
+            for (let z = 0; z < arrEdges.length; z++) {
+                result += arrEdges[z];
+            }
+            console.log('result fate: ' + result);
+        } else {
+//FOR DND
+//create arr of DND edges
+            for (let i = 0; i < dicesNum; i++) {
+                arrEdges.push(Math.floor(Math.random() * (edgesNum - min +1) + min));
+            }
+            console.log('arr-dnd: ' + arrEdges);
+
+//calculate the resultSum of rolled DND edges
+            for (let i = 0; i < arrEdges.length; i++) {
+                result += arrEdges[i];
+            }
+            console.log('result-dnd: ' + result);
+        }
+
+        const createResultOutput = () => {
+            createSymbol(dicesNum, "dice");
+            createSymbol('d', 'special-symbol');
+            createSymbol(edgesNum, 'dice');
+            createSymbol(':', 'special-symbol');
+            for (let i = 0; i < arrEdges.length; i++) {
+                createSymbol(arrEdges[i], 'symbol');
+            }
+            createSymbol('=', 'special-symbol')
+            createSymbol(result, 'result');
+        }
+        playAudio();
+        createResultOutput();
+        clearInput();
+        console.log('final result: ' + result);
+    }
+}
+
+btn.addEventListener('click', rollDices);
--- js/script.js
+++ js/script.js
@@ -1,129 +1,134 @@
-'use strict'
 const btn = document.querySelector('.btn');
 
-function rollDices() {
-    const dicesNum = +document.querySelector('#dices-num').value;
-    const edgesNum = document.querySelector('#edges-num').value;
-    const resultDiv = document.querySelector('.result-div');
-    const min = 0;
-    let result = 0;
-    let resultWrong = 'Error: Enter positive integer num';
-    let arrEdges = [];
+const dicesNum = +document.querySelector('#dices-num').value;
+const edgesNum = document.querySelector('#edges-num').value;
+const resultDiv = document.querySelector('.result-div');
+const min = 0;
+let result = 0;
+let resultWrong = 'Error: Enter positive integer num';
+let arrEdges = [];
 
-// function playAudio {
-    function playAudio () {
+class myDice {
+    constructor() {
+        this.dicesNum = dicesNum;
+        this.edgesNum = edgesNum;
+        this.resultDiv = resultDiv;
+        this.result = result;
+        this.resultWrong = resultWrong;
+        this.arrEdges = arrEdges;
+    }
+
+    playAudio() {
         const audio = new Audio();
         audio.preload = 'auto';
         audio.src = './media/sounds/dice_roll_sound.mp3';
         audio.play();
     }
 
-
-//function validate FateCore
-    function validateFate(edgesNum) {
-        return ['f', 'fate', 'fatecore', 'ф', 'фейт', 'фейткор'].includes(edgesNum.toLowerCase());
+    validateFate() {
+        return ['f', 'fate', 'fatecore', 'ф', 'фейт', 'фейткор'].includes(this.edgesNum.toLowerCase());
     }
 
-//create wrapper-div eachResult
-    const eachResult = document.createElement('div');
-    eachResult.classList.add('each-result');
-    resultDiv.appendChild(eachResult);
+    createWrapperDiv () {
+        this.eachResult = document.createElement('div');
+        this.eachResult.classList.add('each-result');
+        this.resultDiv.appendChild(this.eachResult);
+    }
 
-//function that create input div
-    function createSymbol(symbolName, styleName) {
+    createSymbol (symbolName, styleName) {
         const symbol = document.createElement('div');
         symbol.classList.add('symbol');
         symbol.classList.add(styleName);
-        eachResult.appendChild(symbol);
+        this.eachResult.appendChild(symbol);
         symbol.textContent = symbolName;
     }
 
-//function clear input value
-    function clearInput () {
+    clearInput () {
         document.querySelector('#dices-num').value = '';
         document.querySelector('#edges-num').value = '';
     }
 
-//function for checking validity isInteger
-    function isInteger (num) {
-       return (num % 1 !== 0);
+    isInteger (num) {
+        return (num % 1 !== 0);
     }
 
-//check the validity of the input
-    if (dicesNum <=0 || isNaN(dicesNum) || edgesNum <= 0 || isInteger(dicesNum)) {
-        createSymbol(resultWrong, 'symbol','result');
-        clearInput();
-        console.log('Wrong number');
-    } else if (!validateFate(edgesNum) && isInteger(edgesNum)) {
-        createSymbol(resultWrong, 'symbol','result');
-        clearInput();
-        console.log("EdgesNum isn't integer");
-    } else if (!validateFate(edgesNum) && isNaN(edgesNum)) {
-        createSymbol(resultWrong, 'symbol','result');
-        clearInput();
-        console.log('EdgesNum is NaN');
-    } else {
-//FOR FateCore
-        if (validateFate(edgesNum))
-        {
-            console.log('Start FateCore-mode');
+    roll () {
+        this.dicesNum = +document.querySelector('#dices-num').value;
+        this.edgesNum = document.querySelector('#edges-num').value;
+        this.createWrapperDiv();
+        if (this.dicesNum <= 0 || isNaN(this.dicesNum) || this.edgesNum <= 0 || this.isInteger(this.dicesNum)) {
+            this.createSymbol(this.resultWrong, 'symbol', 'result');
+            this.clearInput();
+            console.log('Wrong number');
+        } else if (!(this.validateFate(this.edgesNum)) && this.isInteger(this.edgesNum)) {
+            this.createSymbol(this.resultWrong, 'symbol', 'result');
+            this.clearInput();
+            console.log("EdgesNum isn't integer");
+        } else if (!(this.validateFate(this.edgesNum)) && isNaN(this.edgesNum)) {
+            this.createSymbol(this.resultWrong, 'symbol', 'result');
+            this.clearInput();
+            console.log('EdgesNum is NaN');
+        } else {
+            if (this.validateFate(this.edgesNum)) {
+                console.log('Start FateCore-mode');
+                for (let i = 0; i < this.dicesNum; i++) {
+                    this.arrEdges.push(Math.floor(Math.random() * (5 - this.min + 1) + this.min));
+                    console.log('arrEdges[i]' + this.arrEdges[i]);
+                }
+                console.log('arr-fate before turning into -1, 0, 1: ' + this.arrEdges);
 
-//create arr of edges for FateCore
-        for (let i = 0; i < dicesNum; i++) {
-            arrEdges.push(Math.floor(Math.random() * (5 - min +1) + min));
-            console.log('arrEdges[i]' + arrEdges[i]);
-        }
-        console.log('arr-fate before turning into -1, 0, 1: ' + arrEdges);
+                for (let y = 0; y < this.arrEdges.length; y++) {
 
-        for (let y = 0; y < arrEdges.length; y++) {
+                    if (this.arrEdges[y] === 0 || this.arrEdges[y] === 1) {
+                        this.arrEdges[y] = -1;
+                    } else if (this.arrEdges[y] === 2 || this.arrEdges[y] === 3) {
+                        arrEdges[y] = 0;
+                    } else if (this.arrEdges[y] === 4 || this.arrEdges[y] === 5) {
+                        this.arrEdges[y] = 1;
+                    } else {
+                        console.log('Something goes wrong');
+                    }
+                }
+                console.log('arr-fate after turning into -1, 0, 1: ' + this.arrEdges);
 
-            if (arrEdges[y] === 0 || arrEdges[y] === 1) {
-                arrEdges[y] = -1;
-            } else if (arrEdges[y] === 2 || arrEdges[y] === 3) {
-                arrEdges[y] = 0;
-            } else if (arrEdges[y] === 4 || arrEdges[y] === 5) {
-                arrEdges[y] = 1;
+                for (let z = 0; z < this.arrEdges.length; z++) {
+                    this.result += this.arrEdges[z];
+                }
+                console.log('result fate: ' + this.result);
             } else {
-                console.log('Something goes wrong');
+                for (let i = 0; i < this.dicesNum; i++) {
+                    this.arrEdges.push(Math.floor(Math.random() * (this.edgesNum - min + 1) + min));
+                }
+                console.log('arr-dnd: ' + this.arrEdges);
+
+                for (let i = 0; i < this.arrEdges.length; i++) {
+                    this.result += this.arrEdges[i];
+                }
+                console.log('result-dnd: ' + this.result);
             }
+            this.createResultOutput();
         }
-        console.log('arr-fate after turning into -1, 0, 1: ' + arrEdges);
-
-        for (let z = 0; z < arrEdges.length; z++) {
-            result += arrEdges[z];
-        }
-        console.log('result fate: ' + result);
-    } else {
-//FOR DND
-//create arr of DND edges
-        for (let i = 0; i < dicesNum; i++) {
-            arrEdges.push(Math.floor(Math.random() * (edgesNum - min +1) + min));
-        }
-        console.log('arr-dnd: ' + arrEdges);
-
-//calculate the resultSum of rolled DND edges
-        for (let i = 0; i < arrEdges.length; i++) {
-            result += arrEdges[i];
-        }
-        console.log('result-dnd: ' + result);
     }
-
-    const createResultOutput = () => {
-        createSymbol(dicesNum, "dice");
-        createSymbol('d', 'special-symbol');
-        createSymbol(edgesNum, 'dice');
-        createSymbol(':', 'special-symbol');
-        for (let i = 0; i < arrEdges.length; i++) {
-            createSymbol(arrEdges[i], 'symbol');
+     createResultOutput  ()  {
+        this.createSymbol(this.dicesNum, "dice");
+        this.createSymbol('d', 'special-symbol');
+        this.createSymbol(this.edgesNum, 'dice');
+        this.createSymbol(':', 'special-symbol');
+        for (let i = 0; i < this.arrEdges.length; i++) {
+            this.createSymbol(this.arrEdges[i], 'symbol');
         }
-        createSymbol('=', 'special-symbol')
-        createSymbol(result, 'result');
-    }
-        playAudio();
-        createResultOutput();
-        clearInput();
-        console.log('final result: ' + result);
+        this.createSymbol('=', 'special-symbol')
+        this.createSymbol(this.result, 'result');
+
+        this.clearInput();
+        this.playAudio();
+        this.arrEdges.length = 0;
     }
 }
 
-btn.addEventListener('click', rollDices);
+function throwingDices() {
+    let dice = new myDice(dicesNum, edgesNum);
+    dice.roll();
+}
+
+btn.addEventListener('click', throwingDices);
